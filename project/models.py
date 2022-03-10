37897be53da5a9b7954ceeb2f55479e765786a80
Alex Clark <aclark@aclark.net>
2022-03-10 11:29:07 -0500

    Update

--- project/models.py
+++ project/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+
 
 
 class TimeEntry(models.Model):
@@ -8,3 +10,4 @@ class TimeEntry(models.Model):
         verbose_name_plural = "Time Entries"
 
     hours = models.DecimalField(decimal_places=2, max_digits=4, default=1)
+    date = models.DateField(default=timezone.now)
