cca83c192cf28bf995016bc10bcc3ec5e7b60301
Alex Clark <aclark@aclark.net>
2022-03-13 19:24:00 -0400

    Update

--- project/urls.py
+++ project/urls.py
@@ -20,7 +20,7 @@ from search import views as search_views
 urlpatterns = [
     path("django-admin/", admin.site.urls),
     path("admin/", include(wagtailadmin_urls)),
-    # path("documents/", include(wagtaildocs_urls)),
+    path("wagtail-documents/", include(wagtaildocs_urls)),
     path("search/", search_views.search, name="search"),
 ]
 
